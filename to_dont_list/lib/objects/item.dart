4828ffed29bfaa576f3dbf58d015c74fda193613
JB7704 <Buchananjs@hendrix.edu>
2026-09-10 13:22:48 -0500

    updated item.dart
    changed item.dart to contain hero and issue number

--- to_dont_list/lib/objects/item.dart
+++ to_dont_list/lib/objects/item.dart
@@ -1,9 +1,11 @@
 // Data class to keep the string and have an abbreviation function
 
 class Item {
-  const Item({required this.name});
+  const Item({required this.name, required this.hero, required this.issueNumber});
 
   final String name;
+  final String hero;
+  final int issueNumber;
 
   String abbrev() {
     return name.substring(0, 1);
